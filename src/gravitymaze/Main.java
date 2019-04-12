2afc9fabceae5331ebbe80e1ee7b37edf3da8f42
callmemars <marcelrmillan@gmail.com>
2019-04-12 04:20:52 -0400

    Add files via upload

---
+++ src/gravitymaze/Main.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package gravitymaze;
+
+/**
+ *
+ * @author Marcel Millan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+    }
+    
+}
--- src/gravitymaze/Vertex.java
+++ src/gravitymaze/Vertex.java
@@ -8,8 +8,37 @@ package gravitymaze;
 
 public class Vertex
 {
-    String label;
-    Vertex(String label) {
-    this.label = label;
-}
+    int x, y, z;
+    
+   public Vertex(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+   
+   public void changeVertex(int x, int y, int z)
+   {
+       this.x = x;
+       this.y = y;
+       this.z = z;
+       
+   }
+   public void setX(int x)
+   {
+       this.x = x;
+   }
+     public void setY(int y)
+   {
+       this.y = y;
+   }
+       public void setZ(int z)
+   {
+       this.z = z;
+   }
+   public void getVertex()
+   {
+       System.out.println("x= " + x + "\ny= " + y + "\nz= " +z);
+   }
+   
 }
